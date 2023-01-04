85d22ab3ccc15004a6af0891ed784abca8dbe2bf
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-04 14:17:10 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let gallery = document.querySelector("#gallery");
+
+gallery.innerHTML += ("<figure><img src='./assets/img/beatch.avif'width=200px><figcaption>Fig 1</figcaption></figure>");
+gallery.innerHTML += ("<figure><img src='./assets/img/beatch.avif'width=200px><figcaption>Fig 2</figcaption></figure>");
+gallery.innerHTML += ("<figure><img src='./assets/img/beatch.avif'width=200px><figcaption>Fig 3</figcaption></figure>");
+
+gallery.style.display="flex";
+
+console.log(gallery);
+
+
+
+
